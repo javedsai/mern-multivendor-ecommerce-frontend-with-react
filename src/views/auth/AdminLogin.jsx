22f22ff495427ed4adb21280c623e40010c44282
Javed <javedsai@gmail.com>
2025-11-04 23:54:23 +0530

    Initial commit where we have setup redux and login form

---
+++ src/views/auth/AdminLogin.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { admin_login, messageClear } from '../../store/Reducers/authReducer';
+import { PropagateLoader } from 'react-spinners';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+
+const AdminLogin = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    })
+
+    const { loader, successMessage, errorMessage } = useSelector(state => state.auth)
+
+    const inputHandler = (e) => {
+        setState({
+            ...state,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+        dispatch(admin_login(state))
+        // console.log(state)
+    }
+
+    const overrideSyle = {
+      display: 'flex',
+      margin: '0 auto',
+      height: '24px',
+      justifyContent: 'center',
+      alignItem: 'center'
+    }
+
+    useEffect(() => {
+      if (errorMessage) {
+        toast.error(errorMessage)
+        dispatch(messageClear())
+      }
+      if (successMessage) {
+        toast.success(successMessage)
+        dispatch(messageClear())
+        navigate('/')
+      }
+    },[errorMessage, successMessage, dispatch, navigate])
+    
+
+  return (
+    <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center' >
+      <div className='w-[350px] text-[#ffffff] p-2'>
+        <div className='bg-[#6f68d1] p-4 rounded-md'>
+
+            <div className='h-[70px] flex justify-center items-center'>
+                <div className='w-[180px] h-[50px]'>
+                    <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="logo" />
+                </div>
+            </div>
+
+          <form onSubmit={submit}>
+            <div className='flex flex-col w-full gap-1 mb-3'>
+              <label htmlFor="email">Email</label>
+              <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type="text" onChange={inputHandler} value={state.email} name='email' placeholder='Email' id='email' required />
+
+            </div>
+
+            <div className='flex flex-col w-full gap-1 mb-3'>
+              <label htmlFor="password">Password</label>
+              <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type="password" onChange={inputHandler} value={state.password} name='password' placeholder='Password' id='password' required />
+            </div>
+
+            <button disabled={loader ? true: false} className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+              {
+                loader ? <PropagateLoader color='#fff' cssOverride={overrideSyle} /> : 'Log In' 
+              }
+            </button>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminLogin
